0d9657f915537e91e9a2e1a8b121e41949f21625
“Eling <>
2021-03-29 19:31:23 -0700

    blog original script

--- scripts/blog-forloop.js
+++ scripts/blog-forloop.js
@@ -1,35 +1,60 @@
-const container = document.getElementById('blogContainer');
-const el = document.createElement('h1');
-el.textContent = 'Adoptadog Blog';
+const blogs = [
+	{
+		headline:"Traveling With Your Dog",
+		text: "Iduciendisite quo magnatem iuntum quid quaest ea am, is dolenem quidustrum fuga.",
+		image: "./images/blog-1.jpg",
+		alt: "a person is walking four dogs on leashes"
+	},
 
-const blogImg = document.createElement('img');
-blogImg.src='./images/blog-1.jpg';
-const blogTitle = document.createElement('h2');
-blogTitle.textContent = 'Traveling With Your Dog';
-const blogText = document.createElement('p');
-blogText.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+	{
+		headline: "How To Walk Multiple Dog",
+		text: "Iduciendisite quo magnatem iuntum quid quaest ea am, dolenem quidustrum fuga.",
+		image: "./images/blog-2.jpg",
+		alt: "a person is walking four dogs on leashes"
+	},
 
-const blogImg2 = document.createElement('img');
-blogImg2.src='./images/blog-2.jpg';
-const blogTitle2 = document.createElement('h2');
-blogTitle2.textContent = 'How To Walk Multiple Dog';
-const blogText2 = document.createElement('p');
-blogText2.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+	{
+		headline: "Teach Your Dog To Fetch!",
+		text: "Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis.",
+		image: "./images/blog-3.jpg",
+		alt: "a woman is teaching a dog to fetch"
+	},
+];
 
-const blogImg3 = document.createElement('img');
-blogImg3.src='./images/blog-3.jpg';
-const blogTitle3 = document.createElement('h2');
-blogTitle3.textContent = 'Teach Your Dog To Fetch!';
-const blogText3 = document.createElement('p');
-blogText3.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+const blogsContainer = document.getElementById('blogs-container');
+console.log(blogsContainer);
 
-blogContainer.appendChild(el);
-blogContainer.appendChild(blogImg);
-blogContainer.appendChild(blogTitle);
-blogContainer.appendChild(blogText);
-blogContainer.appendChild(blogImg2);
-blogContainer.appendChild(blogTitle2);
-blogContainer.appendChild(blogText2);
-blogContainer.appendChild(blogImg3);
-blogContainer.appendChild(blogTitle3);
-blogContainer.appendChild(blogText3);
+for (let i = 0; i < blogs.length; i++) {
+	const blog = blogs[i];
+
+	const dogArticle = document.createElement('article');
+	dogArticle.setAttribute('class', 'blog-article');
+
+		const dogImage = document.createElement('img');
+		dogImage.setAttribute('src', blog.image);
+		dogImage.setAttribute('alt', blog.alt);
+		dogImage.setAttribute('class', 'blog-image');
+
+
+		console.log(dogImage);
+
+		dogArticle.appendChild(dogImage);
+
+		blogsContainer.appendChild(dogArticle);
+
+		const textBox = document.createElement('div');
+		textBox.setAttribute('class', 'blogheader');
+		dogArticle.appendChild(textBox);
+
+		const headline = document.createElement('h2');
+		headline.setAttribute('class', 'margintop');
+		headline.textContent = blog.headline;
+		textBox.appendChild(headline);
+
+		const text = document.createElement('p');
+		text.setAttribute('class', 'p');
+		text.textContent = blog.text;
+		textBox.appendChild(text);
+
+		blogsContainer.appendChild(dogArticle);
+		}
